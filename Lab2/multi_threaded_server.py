3c7fdfccc5e62bdc3dd90622a8565a0b70aa899a
adonoghuTcd <adonoghu@tcd.ie>
2015-10-18 16:02:11 +0100

    Small changes to processing of requests

--- Lab2/multi_threaded_server.py
+++ Lab2/multi_threaded_server.py
@@ -6,13 +6,14 @@ def server_handler(sock, client_addr):
     LF = "\n"
     print('Got connection from', client_addr)
     while True:
-        msg = sock.recv(256)
-        if 'KILL_SERVICE' in msg.decode('utf-8'):
+        msgbytes = sock.recv(256)
+        msg = msgbytes.decode('utf-8')
+        if 'KILL_SERVICE' in msg:
             sock.close()
             return "exit"
-        elif "HELO text"  in msg.decode('utf-8'):
+        elif "HELO"  in msg:
             response = [
-                "HELO text",
+                msg,
                 "IP:"+client_addr,
                 "Port:"+sys.argv[1],
                 "StudentID:aa2d8671e0b9698d706dd81e9fdf63205dcaa89b2926e5df2ec7a594b66861ba",
@@ -24,6 +25,20 @@ def server_handler(sock, client_addr):
             print(msg)
     print('Client closed connection')
     sock.close()
+
+def server(port):
+    pool = ThreadPoolExecutor(5)
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.bind(('', 8000))
+    sock.listen(5)
+    while True:
+        client_sock, client_addr = sock.accept()
+        result = pool.submit(server_handler, client_sock, client_addr)
+       	if result.result() == "exit":
+            print("Killing service")
+            sys.exit()
+server(('',int(sys.argv[1])))
+
 
 def server(port):
     pool = ThreadPoolExecutor(5)
